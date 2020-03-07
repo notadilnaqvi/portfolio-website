d5e6e132ea36db81bc78b63e2be9ae466f89c446
notadilnaqvi <notadilnaqvi@gmail.com>
2020-03-07 13:31:35 +0500

    Testing resume.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import Home from './pages/home.js';
-import About from './pages/about.js';
+import Resume from './pages/resume.js';
 import Contact from './pages/contact.js';
+import About from './pages/about.js';
 import PageNotFound from './pages/error-404.js';
 
 
@@ -11,6 +12,7 @@ function App() {
 	return <Router>
 		<Switch>
 			<Route exact path='/' component={Home}/>
+			<Route exact path='/resume' component={Resume}/>
 			<Route exact path='/contact' component={Contact}/>
 			<Route exact path='/about' component={About}/>
 			<Route exact path='/error-404' component={PageNotFound}/>
---
+++ src/components/resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Pdf from '../media/fakey-dakey.pdf';
+
+
+function Resume() {
+	return(
+		<div>
+			{Pdf}
+		</div>
+	);
+}
+
+export default Resume;

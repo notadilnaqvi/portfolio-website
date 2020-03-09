6bbe98be2e22a05525feefd59f761b12755cb6e4
notadilnaqvi <notadilnaqvi@gmail.com>
2020-03-09 19:12:16 +0500

    Added content to about.js Added .pngs Added header blob

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import CornerBlobs from '../media/blobs/corner.png';
 
 
 function Header() {
 	return(
-		<header>
-			<Link to='/' className='logo'>adilnaqvi<div className='dot'>.</div>com</Link>
-			<nav>
-				<ul className="nav__links">
-					<li><Link to='/Syed Adil Hasan Naqvi Resume.pdf' target="_blank" className='link'>resume</Link></li>
-					<li><Link to='/projects' className='link'>projects</Link></li>
-					<li><Link to='/contact' className='link'>contact</Link></li>
-					<li><Link to='/about' className='link'>about</Link></li>
-				</ul>
-			</nav>
-		</header>
+		<React.Fragment>
+			<img src={CornerBlobs} alt='' className='corner-blobs'/>
+			<header>
+				<Link to='/' className='logo'>adilnaqvi<div className='dot'>.</div>com</Link>
+				<nav>
+					<ul className="nav__links">
+						<li><Link to='/Syed Adil Hasan Naqvi Resume.pdf' target="_blank" className='link'>resume</Link></li>
+						<li><Link to='/projects' className='link'>projects</Link></li>
+						<li><Link to='/contact' className='link'>contact</Link></li>
+						<li><Link to='/about' className='link'>about</Link></li>
+					</ul>
+				</nav>
+			</header>
+			
+		</React.Fragment>
 	);
 }
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,78 @@
 import React from 'react';
 import Header from '../components/header.js';
 import Title from '../components/title.js';
+import FirstBlob from '../media/blobs/first.png';
+import SecondBlob from '../media/blobs/second.png';
+import ThirdBlob from '../media/blobs/third.png';
+import FourthBlob from '../media/blobs/fourth.png';
+import FifthBlob from '../media/blobs/fifth.png';
+import SixthBlob from '../media/blobs/sixth.png';
+import FirstImg from '../media/photo-2.png';
+import SecondImg from '../media/second.png';
+import ThirdImg from '../media/third.png';
+import FourthImg from '../media/fourth.png';
+import FifthImg from '../media/fifth.png';
+import SixthImg from '../media/sixth.png';
+
 
 function About(){
 	return(
 		<div>
 			<Title pageTitle='About'/>
 			<Header/>
-			<hr/>
-			<h1>This is the about page!</h1>
+			<hr/>	
+			<div className="about-flex-wrapper">
+				<h1 className="about-heading">know more about me!</h1>
+				<div className="about-segment">
+				<img src={FirstBlob} alt='' className='first-blob'/>
+					<p className="about-segment-text">
+						My full name is Syed Adil Hasan Naqvi <br/> and I usually go by <span className='blue-ify'>Adil</span>
+					</p>
+					<img src={FirstImg} alt="" height='150px' />
+				</div>
+
+				<div className="about-segment">
+					<img src={SecondBlob} alt='' className='second-blob'/>
+					<img src={SecondImg} alt="" className="about-segment-image"/>
+					<p className="about-segment-text" align='right'>
+						I'm from <span className="blue-ify">Pakistan</span>, <br/> a South Asian country with a <br/> rich culture and an even richer history
+					</p>
+				</div>
+
+				<div className="about-segment">
+					<img src={ThirdBlob} alt='' className='third-blob'/>
+					<p className="about-segment-text">
+						I like playing chess, casual <br/> reading, graphics designing, <br/> and <span className='blue-ify'>coding</span>
+					</p>
+					<img src={ThirdImg} alt="" className="about-segment-image"/>
+				</div>
+
+				<div className="about-segment">
+					<img src={FourthBlob} alt='' className='fourth-blob'/>
+					<img src={FourthImg} alt="" className="about-segment-image"/>
+					<p className="about-segment-text" align='right'>
+						I did my Intermediate from <br/> <span className='blue-ify'>GCU</span> Lahore – one of the most <br/> prestigious colleges in Pakistan
+					</p>
+				</div>
+
+				<div className="about-segment">
+					<img src={FifthBlob} alt='' className='fifth-blob'/>
+					<p className="about-segment-text">
+						I majored in Mechanical Engineering <br/> from <span className='blue-ify'>NUST</span> – the #1 science and technology <br/> university in the country – with a minor in <br/> Computer Science
+					</p>
+					<img src={FifthImg} alt='' className="about-segment-image"/>
+					
+				</div>
+
+				<div className="about-segment">
+					<img src={SixthBlob} alt='' className='sixth-blob'/>
+					<img src={SixthImg} alt='' className="about-segment-image"/>
+					<p className='about-segment-text' align='right'>
+						I am particularly interested in <br/> <span className='blue-ify'>machine learning</span>, <span className='blue-ify'>AI</span> and their <br/> integration in everyday human life
+					</p>
+				</div>
+				<h1 className="about-heading">stuff I enjoy</h1>
+			</div>
 		</div>
 	);
 }

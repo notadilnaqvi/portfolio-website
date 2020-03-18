80eff1a837eee4bf2d76d4836bcaced74acf2bef
notadilnaqvi <notadilnaqvi@gmail.com>
2020-03-19 01:51:05 +0500

    Fixed about.js mobile positioning

--- src/components/knowMore.js
+++ src/components/knowMore.js
@@ -26,55 +26,55 @@ function KnowMore(){
 
 			{/* ME */}
 			<div className="about-segment">
-				<img src={FirstBlob} alt='' className='first-blob'/>
 				<p className="about-segment-text">
-					My full name is Syed Adil Hasan Naqvi <br/> and I usually go by <span className='blue-ify'>Adil</span>
+					<img src={FirstBlob} alt='' className='first-blob'/>
+					My full name is Syed Adil Hasan Naqvi and I usually go by <span className='blue-ify'>Adil</span>
 				</p>
 				<img src={FirstImg} alt="" height='150px' />
 			</div>
 
 			{/* COUNTRY */}
 			<div className="about-segment">
-				<img src={SecondBlob} alt='' className='second-blob'/>
 				<img src={SecondImg} alt="" className="about-segment-image"/>
 				<p className="about-segment-text" align='right'>
-					I'm from <span className="blue-ify">Pakistan</span>, <br/> a South Asian country with a <br/> rich culture and an even richer history
+					<img src={SecondBlob} alt='' className='second-blob'/>
+					I'm from <span className="blue-ify">Pakistan</span>, a South Asian country with a rich culture and an even richer history
 				</p>
 			</div>
 				
 				{/* THINGS I LIKE */}
 			<div className="about-segment">
-				<img src={ThirdBlob} alt='' className='third-blob'/>
 				<p className="about-segment-text">
-					I like playing chess, casual <br/> reading, graphics designing, <br/> and <span className='blue-ify'>coding</span>
+					<img src={ThirdBlob} alt='' className='third-blob'/>
+					I like playing chess, casual reading, graphics designing, and <span className='blue-ify'>coding</span>
 				</p>
 				<img src={ThirdImg} alt="" className="about-segment-image"/>
 			</div>
 
 			{/* INTERMEDIATE */}
 			<div className="about-segment">
-				<img src={FourthBlob} alt='' className='fourth-blob'/>
 				<img src={FourthImg} alt="" className="about-segment-image"/>
 				<p className="about-segment-text" align='right'>
-					I did my Intermediate from <br/> <span className='blue-ify'>GCU</span> Lahore – one of the most <br/> prestigious colleges in Pakistan
+					<img src={FourthBlob} alt='' className='fourth-blob'/>
+					I did my Intermediate from <span className='blue-ify'>GCU</span> Lahore – one of the most prestigious colleges in Pakistan
 				</p>
 			</div>
 
 			{/* MAJOR */}
 			<div className="about-segment">
-				<img src={FifthBlob} alt='' className='fifth-blob'/>
 				<p className="about-segment-text">
-					I majored in Mechanical Engineering <br/> from <span className='blue-ify'>NUST</span> – the #1 science and technology <br/> university in the country – with a minor in <br/> Computer Science
+					<img src={FifthBlob} alt='' className='fifth-blob'/>
+					I majored in Mechanical Engineering from <span className='blue-ify'>NUST</span> – the #1 science and technology university in the country – with a minor in Computer Science
 				</p>
 				<img src={FifthImg} alt='' className="about-segment-image"/>	
 			</div>
 
 			{/* INTERESTS */}
 			<div className="about-segment">
-				<img src={SixthBlob} alt='' className='sixth-blob'/>
 				<img src={SixthImg} alt='' className="about-segment-image"/>
 				<p className='about-segment-text' align='right'>
-					I am particularly interested in <br/> <span className='blue-ify'>machine learning</span>, <span className='blue-ify'>AI</span> and their <br/> integration in everyday human life
+					<img src={SixthBlob} alt='' className='sixth-blob'/>
+					I am particularly interested in <span className='blue-ify'>machine learning</span>, <span className='blue-ify'>AI</span> and their integration in everyday human life
 				</p>
 			</div>
 		</div>

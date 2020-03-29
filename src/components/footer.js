9d77eff97c7213731ef1e7835496f40ab1a9afdf
notadilnaqvi <notadilnaqvi@gmail.com>
2020-03-29 20:38:59 +0500

    Added license phrase to footer Minified PNGs

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,16 @@ function Footer(){
 			<div className='footer-text'>
 				<div>Copyright Ⓒ 2020 Adil Naqvi.&nbsp;</div>
 				<div>All Rights Reserved.&nbsp;</div>
+				<div>
+					<a 
+						href="https://github.com/notadilnaqvi/portfolio-website/blob/master/LICENSE"
+						className = "license"
+						target = "_blank"
+						rel="noopener noreferrer"
+					>
+						Open source under MIT License.&nbsp;
+					</a>
+				</div>
 				<div>Made with <span role="img" aria-label="heart">❤️</span> in React.</div>
 			</div>
 		</footer>
@@ -17,16 +27,3 @@ function Footer(){
 
 
 export default Footer;
-
-// function StickyFooter(){
-// 	return(
-// 		<footer className='sticky-footer'>
-// 			<img src={BottomBlobs} alt='' className='bottom-blobs'/>
-// 			<div className='footer-text'>
-// 				<div>Copyright Ⓒ 2020 Adil Naqvi.&nbsp;</div>
-// 				<div>All Rights Reserved.&nbsp;</div>
-// 				<div>Made with <span role="img" aria-label="heart">❤️</span> in React.&nbsp;</div>
-// 			</div>
-// 		</footer>
-// 	);
-// }

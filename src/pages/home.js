ecf3105629927ec4f7c311fd0a2cb3115122fd70
Adil Naqvi <notadilnaqvi@gmail.com>
2020-04-27 01:32:59 +0500

    Removed Rick Rollin'

--- src/pages/home.js
+++ src/pages/home.js
@@ -42,7 +42,7 @@ function Home(){
 					<img src={SecondBlob} alt="" className='second-blob'/>
 					<p align='right'>
 						I'm very passionate when it comes to <br/> programming. I have <span className='blue-ify'>3 specializations</span> and a <br/> minor in computer science <br/>
-						<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" targer='_black' className='link'>view specializations</a>
+						<a href="#" targer='_black' className='link'>view specializations</a>
 					</p>
 					
 				</div>

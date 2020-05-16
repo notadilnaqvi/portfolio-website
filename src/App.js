753c8e2031216510c039eff78cc74db3382fe7fa
Adil Naqvi <notadilnaqvi@gmail.com>
2020-05-16 23:58:35 +0500

    Added temporary Uses page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom
 import Home from './pages/home.js';
 import Projects from './pages/projects.js';
 import Contact from './pages/contact.js';
+import Uses from './pages/uses.js';
 import About from './pages/about.js';
 import PageNotFound from './pages/error.js';
 import ScrollToTop from './components/scrollToTop.js';
@@ -18,6 +19,7 @@ function App() {
 				<Route exact path='/projects' component={Projects}/>
 				<Route exact path='/contact' component={Contact}/>
 				<Route exact path='/about' component={About}/>
+				<Route exact path='/uses' component={Uses}/>
 				<Route exact path='/error' component={PageNotFound}/>
 				<Redirect to='/error'/>
 			</Switch>
---
+++ src/pages/uses.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Title from '../components/title.js';
+import Footer from '../components/footer.js';
+import Header from '../components/header.js';
+
+
+function Uses(){
+	return(
+		<>
+			<Title pageTitle='/uses'/>
+			<Header/>
+			<hr/>
+            <div className="uses-wrapper">
+                <h1 className="uses-heading">My web development tools</h1>
+                <p className="uses-sub-heading">Inspired by <a href="https://wesbos.com/" className="uses-sub-heading-link">Wes Bos</a></p>
+                <ul>
+                    <li className="uses-li"><b>Editor:</b> VS Code</li>
+                    <li className="uses-li"><b>Terminal:</b> Windows Terminal</li>
+                    <li className="uses-li"><b>Prototyping:</b> Figma</li>
+                    <li className="uses-li"><b>Git GUI:</b> Git Kraken</li>
+                    <li className="uses-li"><b>Hosting:</b> Netlify</li>
+                    <li className="uses-li"><b>Vector graphics:</b> InkScape</li>
+                    <li className="uses-li"><b>Machine:</b> Dell Inspiron 15 - 4 GB Core i3 1.9 GHz</li>
+                </ul>
+            </div>
+
+			<Footer/>
+		</>
+	);
+}
+
+export default Uses;

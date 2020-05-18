fb45e91d2be3c22990d61cb88296d273837bf51f
Adil Naqvi <notadilnaqvi@gmail.com>
2020-05-18 20:54:10 +0500

    Removed license link from footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,16 +9,7 @@ function Footer(){
 			<div className='footer-text'>
 				<div>Copyright Ⓒ 2020 Adil Naqvi.&nbsp;</div>
 				<div>All Rights Reserved.&nbsp;</div>
-				<div>
-					<a 
-						href="https://github.com/notadilnaqvi/portfolio-website/blob/master/LICENSE"
-						className = "license"
-						target = "_blank"
-						rel="noopener noreferrer"
-					>
-						Open source under MIT License.&nbsp;
-					</a>
-				</div>
+				<div>Open source under MIT License.&nbsp;</div>
 				<div>Made with <span role="img" aria-label="heart">❤️</span> in React.</div>
 			</div>
 		</footer>

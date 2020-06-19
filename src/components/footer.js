93c0b8029147cd647035f17317f36a2e6969cc2b
Adil Naqvi <notadilnaqvi@gmail.com>
2020-06-19 17:57:55 +0500

    Changed copyright symbol

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import BottomBlobs from '../media/blobs/bottom.png';
+import React from "react";
+import BottomBlobs from "../media/blobs/bottom.png";
 
-
-function Footer(){
-	return(
-		<footer className='footer'>
-			<img src={BottomBlobs} alt='' className='bottom-blobs'/>
-			<div className='footer-text'>
-				<div>Copyright Ⓒ 2020 Adil Naqvi.&nbsp;</div>
+function Footer() {
+	return (
+		<footer className="footer">
+			<img src={BottomBlobs} alt="" className="bottom-blobs" />
+			<div className="footer-text">
+				<div>Copyright &copy; 2020 Adil Naqvi.&nbsp;</div>
 				<div>All Rights Reserved.&nbsp;</div>
 				<div>Open source under MIT License.&nbsp;</div>
-				<div>Made with <span role="img" aria-label="heart">❤️</span> in React.</div>
+				<div>
+					Made with{" "}
+					<span role="img" aria-label="heart">
+						❤️
+					</span>{" "}
+					in React.
+				</div>
 			</div>
 		</footer>
 	);
 }
-
 
 export default Footer;
